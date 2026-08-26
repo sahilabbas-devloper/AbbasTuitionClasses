3dccde439c46fd6e5d24e15539b39d8dfb3ed4cb
Sahil Abbas <sa9300432@gmail.com>
2026-08-26 13:02:22 +0530

    fix gallery

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,5 +1,24 @@
 import React, { useState, useEffect, useRef } from 'react';
-import { galleryData } from '../data/galleryData.js';
+
+
+
+const galleryData = [
+  { id: 1, title: "Classroom Session", category: "Classroom", src: "/Classroom.jpeg" },
+  { id: 2, title: "Annual Function", category: "Events", src: "/Functions.jpeg" },
+  { id: 3, title: "Topper Felicitation", category: "Achievements", src: "/Topperpicture.jpeg" },
+  { id: 4, title: "Weekly Test", category: "Classroom", src: "/Weeklytest.jpeg" },
+  { id: 5, title: "Annual Function", category: "Events", src: "/Functions1.jpeg" },
+  { id: 6, title: "Prize Distribution", category: "Achievements", src: "/Medal.jpeg" },
+  { id: 7, title: "Prize Distribution", category: "Achievements", src: "/Medal2.jpeg" },
+  { id: 8, title: "Prize Distribution", category: "Achievements", src: "/Medal3.jpeg" },
+  { id: 9, title: "Prize Distribution", category: "Achievements", src: "/Medal4.jpeg" },
+  { id: 10, title: "Class 10th batch", category: "Batches", src: "/Class10bactch.jpeg" },
+  { id: 11, title: "batch 1", category: "Batches", src: "/batch1.jpeg" },
+  { id: 12, title: "batch 2", category: "Batches", src: "/Batch2.jpeg" },
+  { id: 13, title: "batch 3", category: "Batches", src: "/batch3.jpeg" },
+  { id: 14, title: "batch 4", category: "Batches", src: "/batch4.jpeg" },
+  // Nayi photos aane par bas yahan naya object jodna hai
+];
 
 const Gallery = () => {
   const [selectedCategory, setSelectedCategory] = useState('All');
